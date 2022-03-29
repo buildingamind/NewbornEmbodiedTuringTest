d1f14184ab978f2e97a8edfa090b09381b69ce1b
denizhanpak <denpak@iu.edu>
2022-03-29 01:36:50 -0400

    F

--- Analysis/run.py
+++ Analysis/run.py
@@ -3,11 +3,18 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import argparse
 
-parser = argparse.ArgumentParser(description='Generate Analysis of agents')
+parser = argparse.ArgumentParser(description='Generate Analysis of agents. Will return nothing unless desired plot is specified.')
 parser.add_argument('-r','--train', help='whether to plot the training of the agents',action='store_true')
 parser.add_argument('-e','--test', help='whether to plot the test trials of the agents',action='store_true')
+parser.add_argument('--run_id', help='Name of the agent to analyze',required=True)
 
 args = parser.parse_args()
+
+def read_log(name,version="train"):
+    path = f"../Data/{name}/logs/{version}/ChickAgent.txt"
+    log = pd.read_csv(path, skipinitialspace=True)
+    return log
+
 def average_three_region(log,column='agent.x',transient=90):
     log.loc[log.Episode % 2 == 1, column] *= -1
     #Translate coordinates
@@ -56,14 +63,20 @@ def average_in_episode_three_region(log,column='agent.x',transient=90):
     return rv
 
 
-#if args.test
+if args.test:
+    log = read_log(args.run_id, version="test")
+    preformance = average_three_region(log)
+    plt.barplot(preformance)
+    plt.show()
 
-rv = []
-rv.append(average_in_episode_three_region(df))
 
-for val in rv:
-    kernel_size = 100
-    kernel = np.ones(kernel_size)/kernel_size
-    val = np.convolve(val,kernel,mode='valid')
-    plt.plot(val,alpha=0.3)
-plt.show()
+if args.train:
+    rv = []
+    rv.append(average_in_episode_three_region(df))
+
+    for val in rv:
+        kernel_size = 100
+        kernel = np.ones(kernel_size)/kernel_size
+        val = np.convolve(val,kernel,mode='valid')
+        plt.plot(val,alpha=0.3)
+    plt.show()
