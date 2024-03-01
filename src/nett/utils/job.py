4be18d9bb664c32ccc660b7a8ae2e74d4d89e906
Zach-Attach <zachlaborde93@gmail.com>
2024-03-01 16:53:28 -0500

    Minor fix

--- src/nett/utils/job.py
+++ src/nett/utils/job.py
@@ -5,8 +5,9 @@ class Job:
     self.device: int = device
     self.condition: str = condition
     self.brain_id: int = brain_id
-    self.paths: dict[str, Path] = self._configure_paths(brain_id, condition)
     self.dir: Path = dir
+    self.paths: dict[str, Path] = self._configure_paths(brain_id, condition)
+    
   
   def _configure_paths(self, brain_id: int, condition: str) -> dict[str, Path]:
     SUBDIRS = ["model", "checkpoints", "plots", "logs", "env_recs", "env_logs"]
