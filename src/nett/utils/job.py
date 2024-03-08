77fcd765b03f41c1459a5d7da125c416e3cc2951
Zach-Attach <zachlaborde93@gmail.com>
2024-03-08 16:50:31 -0500

    Added docstrings to job.py

--- src/nett/utils/job.py
+++ src/nett/utils/job.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 class Job:
   def __init__(self, brain_id: int, condition: str, device: int, dir: Path) -> None:
+    """initialize job"""
     self.device: int = device
     self.condition: str = condition
     self.brain_id: int = brain_id
@@ -10,6 +11,15 @@ class Job:
 
 
   def _configure_paths(self, brain_id: int, condition: str) -> dict[str, Path]:
+    """Configure Paths for the job
+
+    Args:
+        brain_id (int): id for the brain
+        condition (str): condition for the job
+
+    Returns:
+        dict[str, Path]: dictionary of the paths
+    """
     SUBDIRS = ["model", "checkpoints", "plots", "logs", "env_recs", "env_logs"]
     job_dir = Path.joinpath(self.dir, condition, f"brain_{brain_id}")
     return {subdir: Path.joinpath(job_dir, subdir) for subdir in SUBDIRS}
