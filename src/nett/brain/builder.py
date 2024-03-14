979a7b4cbd80c4baa1dcb8a828e39f18d7ec6546
Zach-Attach <zachlaborde93@gmail.com>
2024-03-13 21:52:30 -0400

    Update progress bar initialization in multiBarCallback

--- src/nett/brain/builder.py
+++ src/nett/brain/builder.py
@@ -45,7 +45,7 @@ class multiBarCallback(ProgressBarCallback):
     def _on_training_start(self) -> None:
         # Initialize progress bar
         # Remove timesteps that were done in previous training sessions
-        self.pbar = tqdm(total=self.locals["total_timesteps"], position=self.index)
+        self.pbar = tqdm(total=self.model.num_timesteps, position=self.index)
         # self.pbar = tqdm(total=self.locals["total_timesteps"] - self.model.num_timesteps, position=self.index)
 
 class Brain:
