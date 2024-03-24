51e230e2d092b21849f4cd053b04eaea678c2641
Zach-Attach <zachlaborde93@gmail.com>
2024-03-24 14:10:20 -0400

    Fix copy function call in NETT class

--- src/nett/nett.py
+++ src/nett/nett.py
@@ -246,7 +246,7 @@ class NETT:
 
         # assign devices based on memory availability
         # get the list of devices
-        free_devices: list[int] = copy.copy(self.devices)
+        free_devices: list[int] = copy(self.devices)
 
         # get the free memory status for each device
         free_device_memory: dict[int, int] = {device: memory_status["free"] for device, memory_status in self._get_memory_status().items()}
