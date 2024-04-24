914739ff9070d2edd663c35a847fa812897c74b9
Zach-Attach <zachlaborde93@gmail.com>
2024-04-24 18:39:30 -0400

    Added error handling

--- src/nett/nett.py
+++ src/nett/nett.py
@@ -338,9 +338,11 @@ class NETT:
                     device=job.device,
                     index=job.index,
                     paths=job.paths)
-            finally:
-                # close environment
                 train_environment.close()
+            except Exception as e:
+                self.logger.error(f"Error in testing: {e}")
+                train_environment.close()
+                exit()    
 
         # for test
         if self.mode in ["test", "full"]:
@@ -360,9 +362,11 @@ class NETT:
                     iterations=iterations,
                     model_path=str(job.paths['model'].joinpath('latest_model.zip')),
                     index=job.index)
-            finally:
-                # close environment
                 test_environment.close()
+            except Exception as e:
+                self.logger.error(f"Error in testing: {e}")
+                test_environment.close()
+                exit()
 
         return f"Job Completed Successfully for Brain #{job.brain_id} with Condition: {job.condition}"
 
