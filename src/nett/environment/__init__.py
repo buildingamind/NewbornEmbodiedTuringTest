1a04cc11dff5b0c8f5dad2f936f14d4fb5a5a185
Zachary Laborde <zachlaborde93@gmail.com>
2024-06-13 17:07:04 -0500

    Added Option to Run Single Environmental Condition (#110)
    * Added conditions argument to nett.run()
    * Changed list functions to return lists
    * Fixed training error message to say training and not testing
    * Added checks in analysis for existence of necessary files
    * Simplified imports of configs into environment

--- src/nett/environment/__init__.py
+++ src/nett/environment/__init__.py
@@ -1,6 +1,4 @@
 """
 Initializes the environment module.
 """
-# simplify imports
-from nett.environment.configs import list_configs
-from nett.environment.configs import IdentityAndView, Parsing, Slowness, Smoothness, Binding, OneShotViewInvariant
+from nett.environment.configs import *
