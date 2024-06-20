a73d393806ce401781e3d4269e162b83248a3c77
Jake Peacock <wjpeacoc@iu.edu>
2024-06-20 11:04:39 -0400

    doing some preparation for running biomotion experiements. It appears that the .yamls and Brain-Body-Env strats for runnin experiments are quite redundant. hm...

---
+++ src/experiments/parsing.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+from nett import Body, Brain, Environment
+from nett import NETT
+
+brain = Brain(
+    policy="CnnPolicy",
+    algorithm="PPO",
+    train_encoder=True,
+    encoder="resnet10"
+)
+
+body = Body(
+    type = "basic",
+    wrappers = [],
+    dvs = False
+)
+
+environment = Environment(
+    config = "parsing",
+    executable_path = "home/wjpeacoc/nett_env/builds/parsing2/parsing.x86_64"
+)
+
+benchmarks = NETT(
+    brain = brain,
+    body = body,
+    environment = environment
+)
+
+job_sheet = benchmarks.run(
+    output_dir="/data/wjpeacoc/experiments/results/parsing",
+    num_brains=1,
+    train_eps=1,
+    test_eps=1
+)
+
+benchmarks.analyze(
+    config = "parsing",
+    run_dir="/data/wjpeacoc/experiments/results/parsing",
+    output_dir = "/data/wjpeacoc/experiments/results/parsing/analysis"
+)
