3b9890a02abc46755ab90bee98c0369ae7e4d6f4
Zach-Attach <zachlaborde93@gmail.com>
2024-10-18 19:12:03 -0400

    Bug fix for vit

--- src/nett/brain/encoders/disembodied_models/vit_contrastive.py
+++ src/nett/brain/encoders/disembodied_models/vit_contrastive.py
@@ -17,18 +17,9 @@ class VisionTransformer(nn.Module):
     def __init__(self, config):
         super().__init__()
         self.model = ViT(
-            image_size = config.image_size,
-            patch_size = config.patch_size,
-            num_classes = config.num_classes,
-            dim = config.hidden_size,
-            depth = config.num_hidden_layers,
-            heads = config.num_attention_heads,
-            mlp_dim = config.intermediate_size,
             pool='cls',
-            channels=config.channels,
             dim_head=64,
-            dropout = config.hidden_dropout_prob,
-            emb_dropout = config.attention_probs_dropout_prob
+            **config
         )
 
     @torch.no_grad()
