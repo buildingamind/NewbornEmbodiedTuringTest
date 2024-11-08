15d331f1c6dd20149babb306386c54b39e227cd5
Zach-Attach <zachlaborde93@gmail.com>
2024-11-08 16:11:07 -0500

    Added learning_rate and ent_coef params to Brain

--- src/nett/brain/builder.py
+++ src/nett/brain/builder.py
@@ -60,6 +60,8 @@ class Brain:
         reward: str = "supervised",
         batch_size: int = 512,
         buffer_size: int = 2048,
+        learning_rate: float = 0.0003,
+        ent_coef: float = 0,
         train_encoder: bool = True,
         seed: int = 12,
         custom_encoder_args: dict[str, Any]= {},
@@ -82,6 +84,8 @@ class Brain:
         self.embedding_dim = embedding_dim
         self.batch_size = batch_size
         self.buffer_size = buffer_size
+        self.learning_rate = learning_rate
+        self.ent_coef = ent_coef
         self.seed = seed
         self.custom_encoder_args = custom_encoder_args
         self.custom_policy_arch = custom_policy_arch
@@ -127,6 +131,8 @@ class Brain:
                 batch_size=self.batch_size,
                 n_steps=self.buffer_size,
                 verbose=1,
+                learning_rate=self.learning_rate,
+                ent_coef=self.ent_coef,
                 policy_kwargs=policy_kwargs,
                 device=f"cuda:{job.device}")
             
