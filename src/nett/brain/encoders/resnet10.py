f4acf001a1dcf9b41c5cc7d587bb87231bcb6c55
Zach-Attach <zachlaborde93@gmail.com>
2024-11-08 16:08:38 -0500

    bug fix for resnet10

--- src/nett/brain/encoders/resnet10.py
+++ src/nett/brain/encoders/resnet10.py
@@ -1,13 +1,10 @@
 """
 Resnet10CNN feature extractor for stable-baselines3
 """
-import pdb
 import gym
-
 
 import torch as th
 import torch.nn as nn
-import torchvision
 
 from stable_baselines3.common.torch_layers import BaseFeaturesExtractor
 
