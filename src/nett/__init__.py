b90ee4fc241e4c054d72355fd93139a14a4bbf9a
Zach-Attach <zachlaborde93@gmail.com>
2024-12-23 14:08:37 -0500

    Fix to permissions check

--- src/nett/__init__.py
+++ src/nett/__init__.py
@@ -29,7 +29,7 @@ from .brain.params import list_algorithms, list_encoders, list_policies, list_re
 
 # set up logging
 logging.basicConfig(format="[%(name)s] %(levelname)s:  %(message)s", level=logging.INFO)
-logger = logging.getLogger('nett')
+logger = logging.getLogger("nett")
 
 # Alter permissions for ml-agents binaries which are shared between users
 for tmp_dir in [
@@ -38,12 +38,12 @@ for tmp_dir in [
     "/tmp/ml-agents-binaries/tmp",
 ]:
     # check if directory is correct permission
-    if os.stat(tmp_dir).st_mode % 0o1000 == 0o777:
-        # check if directory can be written to
-        if os.access(tmp_dir, os.W_OK):
+    if os.stat(tmp_dir).st_mode % 0o1000 != 0o777:
+        # check if directory is owned by user
+        if os.stat(tmp_dir).st_uid == os.getuid():
             # change permission of directory
             os.chmod(tmp_dir, 0o1777)
         else:
-            logger.warning(
-                f"You do not have permission to change the necessary files in '{tmp_dir}'."
+            logger.error(
+                f"Error: '{tmp_dir}' does not have correct permissions and cannot be changed. If you have superuser access, you can run the following command to change the permissions: 'sudo chmod 1777 {tmp_dir}'. Otherwise, request {os.stat(tmp_dir).st_uid} to run 'chmod 1777 {tmp_dir}'."
             )
