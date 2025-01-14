4d02cbdbda00b5148af8c6f1eb7acdbfb30956f5
Zach-Attach <zachlaborde93@gmail.com>
2025-01-14 14:03:38 -0500

    Cleanup to loading config

--- src/nett/nett.py
+++ src/nett/nett.py
@@ -31,56 +31,6 @@ def validate_conditions(all_conditions: list[str], conditions: Optional[list[str
     else:
         return conditions
 
-
-def validate_executable_path(executable_path: str) -> str:
-    """
-    Validates the Unity executable path.
-
-    Args:
-        executable_path (str): The path to the Unity executable file.
-
-    Returns:
-        str: The validated path to the Unity executable file.
-
-    Raises:
-        ValueError: If the executable path is not a string.
-        FileNotFoundError: If the executable path does not exist.
-        ValueError: If the executable path is not a valid Unity executable file.
-        FileNotFoundError: If the directory does not contain the 'UnityPlayer.so' file.
-        FileNotFoundError: If the data directory does not exist.
-    """
-    if not isinstance(executable_path, str):
-        raise ValueError(
-            f"executable_path should be a string. Instead, it is of type {type(executable_path)}"
-        )
-
-    executable_path: Path = Path(executable_path)
-    unityplayer_path: Path = executable_path.with_name("UnityPlayer.so")
-    datadir_path: Path = executable_path.with_name(executable_path.stem + "_Data")
-
-    # check if executable is correct filetype
-    if executable_path.suffix not in [".x86_64", ".x86"]:
-        raise ValueError(f"{executable_path} is not a valid Unity executable file")
-
-    # check if the executable path exists
-    if not executable_path.is_file():
-        raise FileNotFoundError(f"{executable_path} does not exist")
-
-    # check if the directory contains the 'UnityPlayer.so' file
-    if not unityplayer_path.is_file():
-        raise FileNotFoundError(
-            f"The directory {executable_path} does not contain the file 'UnityPlayer.so'. This may not be a valid Unity executable."
-        )
-
-    # check if the data directory exists
-    if not datadir_path.is_dir():
-        raise FileNotFoundError(
-            f"Expected {datadir_path} to exist in executable directory, but it does not exist. Please check that the path to the Unity executable is correct and that the data directory and executable use the same naming convention."
-        )
-
-    return executable_path
-
-
 def get_experiment_design(executable_path: Path) -> tuple[int, list[str]]:
     """
     Gets the experiment design from the executable directory.
@@ -135,7 +85,7 @@ class NETT:
     """
 
     def __init__(
-        self, config: Path | str | list[Path | str] | dict | list[dict] = None
+        self, config: Path | str | dict = None
     ) -> None:
         """
         Initialize the NETT class.
@@ -144,20 +94,13 @@ class NETT:
         self.logger = logging.getLogger("nett.NETT")
 
         try:
-            if isinstance(config, list):
-                raise NotImplementedError(
-                    "Multiple config files are not supported yet."
-                )
+            if isinstance(config, dict):
+                self.config = config
             elif isinstance(config, (str, Path)):
                 with open(config, "r") as file:
-                    config_text = yaml.safe_load(file)
+                    self.config = yaml.safe_load(file)
 
-            self.brain_config = config_text.get("Brain", {})
-            self.body_config = config_text.get("Body", {})
-            self.environment_config = config_text.get("Environment", {})
-
-            run_config = config_text.get("Run", {})
-            self.run(**run_config)
+            self.run(**self.config["Run"])
         except Exception as e:
             self.logger.exception("Error in loading config")
             raise e
@@ -201,18 +144,17 @@ class NETT:
             >>> task_sheet = benchmarks.run(output_dir="./test_run", num_brains=2, train_eps=100, test_eps=10) # benchmarks is an instance of NETT
         """
 
-        Body.initialize(**self.body_config)
+        ## Initialization ##
+
+        Brain.initialize(**self.config["Brain"])
+        Body.initialize(**self.config["Body"])
+        Environment.initialize(**self.config["Environment"])
 
         ## Input Validation ##
 
-        # validate executable path
-        executable_path: Path = validate_executable_path(
-            self.environment_config["executable_path"]
-        )
-
         # get experiment design
         num_test_conditions, valid_imprinting_conditions = get_experiment_design(
-            executable_path
+            Environment.executable_path
         )
 
         # validate conditions
@@ -221,11 +163,9 @@ class NETT:
         # validate mode
         modes = validate_mode(mode)
 
-        Brain.initialize(**self.brain_config)
+        ## Setup ##
 
         Brain.calc_run_info(num_brains, num_test_conditions, conditions)
-
-        Environment.initialize(**self.environment_config)
 
         Environment.adjust_to_agent(
             steps_per_episode=Brain.steps_per_episode,
