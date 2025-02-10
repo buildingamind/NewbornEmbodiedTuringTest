3dd54bdc4f17e592f37b8bc9daf93a5909f40d2d
Zach-Attach <zachlaborde93@gmail.com>
2025-02-10 15:54:40 -0500

    Updated to allow for json configs as well

--- src/nett/utils/validate.py
+++ src/nett/utils/validate.py
@@ -1,13 +1,20 @@
+import json
 import jsonschema
 from pathlib import Path
 import yaml
+
 
 def validate_config(config: Path | str | dict, schema: dict) -> dict:
     if isinstance(config, dict):
         valid_config = config
     if isinstance(config, (str, Path)):
         with open(config, "r") as file:
-            valid_config = yaml.safe_load(file)
+            if config.endswith(".yaml"):
+                valid_config = yaml.safe_load(file)
+            elif config.endswith(".json"):
+                valid_config = json.load(file)
+            else:
+                raise TypeError("Config should be a yaml or json file.")
     else:
         raise TypeError("Configs should be type dict, str or Path.")
     jsonschema.validate(valid_config, schema)
