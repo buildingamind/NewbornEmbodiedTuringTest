58982d1b12688817c9f9655b36b0ac7b9ec3caee
Zach-Attach <zachlaborde93@gmail.com>
2025-03-06 17:42:40 -0500

    Move around changes to test_viz

--- src/nett/analysis/utils/test_vizOld.py
+++ src/nett/analysis/utils/test_vizOld.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-from scipy import stats
+from scipy.stats import ttest_1samp
 from matplotlib.lines import Line2D
 
 # bar chart colors
@@ -41,57 +41,68 @@ def sort_cond(data, bar_order: str) -> list[str]:
                 .index.tolist()
             )
         case "default":
-            order = [x.strip() for x in bar_order.split(",")]
-        case _:
             order = data["test.cond"].unique().tolist()
+        case _:
+            order = [x.strip() for x in bar_order.split(",")]
 
     return pd.Categorical(data["test.cond"], categories=order, ordered=True)
 
 
-def _stats(group, column="percent_correct", mu=0.5):
-    data = group[column]
-    avgs = data.mean()
-    sd = data.std()
-    count = data.count()
-    se = sd / np.sqrt(count) if count > 0 else np.nan
-    tval, pval = stats.ttest_1samp(data.dropna(), mu) if count > 0 else (np.nan, np.nan)
-    df = count - 1
-    cohensd = (avgs - mu) / sd if sd != 0 else np.nan
-    return pd.Series(
-        {
-            "avgs": avgs,
-            "sd": sd,
-            "count": count,
-            "tval": tval,
-            "df": df,
-            "pval": pval,
-            "se": se,
-            "cohensd": cohensd,
-        }
+def _stats(grouped_data, groups, column="percent_correct", mu=0.5):
+    data = grouped_data[column]
+    stats = (
+        {groups[0]: str(grouped_data.name)}
+        if len(groups) == 1
+        else {k: str(v) for k, v in zip(groups, grouped_data.name)}
+    )
+    stats["avgs"] = avgs = data.mean()
+    stats["sd"] = sd = data.std()
+    stats["count"] = count = data.count()
+
+    stats["tval"], pval = (
+        ttest_1samp(data.dropna(), mu) if count > 0 else (np.nan, np.nan)
+    )
+    stats["df"] = count - 1
+    stats["pval"] = pval
+    stats["se"] = sd / np.sqrt(count) if count > 0 else np.nan
+    stats["cohensd"] = (avgs - mu) / sd if sd != 0 else np.nan
+
+    return pd.Series(stats)
+
+
+def _stats_by_group(data, groups, column="percent_correct", mu=0.5):
+    return (
+        data.groupby(groups, observed=True)  # group data by groups
+        .apply(
+            lambda g: _stats(g, groups, column, mu)
+        )  # apply _stats to each group, resulting in a single row per group
+        .reset_index(drop=True)  # reset the index to clean up the DataFrame
     )
 
 
-def compute_stats(data: pd.DataFrame, results_dir: Path) -> pd.DataFrame:
-    grouped = data.groupby(["imprint.cond", "agent", "test.cond"])
-    by_test_cond = grouped.apply(_stats).reset_index()
-    by_test_cond["imp_agent"] = (
-        by_test_cond["imprint.cond"] + "_" + by_test_cond["agent"].astype(str)
-    )
+def _compute_agent_stats(data: pd.DataFrame, results_dir: Path) -> pd.DataFrame:
+    by_test_cond = _stats_by_group(data, ["imprint.cond", "agent", "test.cond"])
     by_test_cond.to_csv(results_dir / "stats_by_agent.csv", index=False)
     return by_test_cond
 
 
-def make_bar_charts(
-    data, dots, y_col, error_min_col, error_max_col, img_name, chick_data, color_bars
-):
+def make_bar_charts(data, dots, y_col, img_name, chick_data, color_bars):
     plt.figure(figsize=(6, 6))
     sns.set_style("white")
     ax = plt.gca()
     # Hide the right and top spines
     ax.spines[["right", "top"]].set_visible(False)
 
+    # Reorder rows so "Rest" comes first
+    data = data.sort_values(
+        by="test.cond",
+        key=lambda col: (col != "Rest").astype(
+            int
+        ),  # Assign 0 to "Rest" and 1 to others
+    ).fillna(0)
+
     y = data[y_col]
-    yerr = [data[y_col] - data[error_min_col], data[error_max_col] - data[y_col]]
+    yerr = [data[y_col] - data["error_min"], data["error_max"] - data[y_col]]
 
     x_categories = data["test.cond"].unique()
 
@@ -103,7 +114,7 @@ def make_bar_charts(
         color_mapping = dict(zip(x_categories, CUSTOM_PALETTE))
         colors = data["test.cond"].map(color_mapping)
     else:
-        colors = "gray45"
+        colors = "#737373"
 
     x_pos = np.arange(len(x_categories))
     ax.bar(x_pos, y, yerr=yerr, color=colors, capsize=14, width=0.7, linewidth=0)
@@ -118,17 +129,16 @@ def make_bar_charts(
         chick_data_filtered["test.cond"] = pd.Categorical(
             chick_data_filtered["test.cond"], categories=x_categories, ordered=True
         )
-        chick_x_pos = chick_data_filtered["test.cond"].cat.codes - 1
+        chick_x_pos = chick_data_filtered["test.cond"].cat.codes  # - 1
 
+        # shift the chick data to the right of Rest
+        # shift = int(np.isin("Rest", x_categories))
         shift = 0
-        for xi, yi, yerr, i in zip(
+        for xi, yi, yerr in zip(
             chick_x_pos,
             chick_data_filtered["avg"],
             chick_data_filtered["avg_dev"],
-            range(len(chick_x_pos)),
         ):
-            if x_categories[i] == "Rest":
-                shift = 1
             rect = plt.Rectangle(
                 (xi + shift - 0.35, yi - yerr),
                 0.7,
@@ -148,19 +158,15 @@ def make_bar_charts(
             ax.add_line(line)
 
     if dots is not None and "test.cond" in dots.columns:
+        dots = dots.fillna(0)
         dots["test.cond"] = pd.Categorical(
             dots["test.cond"], categories=x_categories, ordered=True
         )
         dot_x_pos = dots["test.cond"].cat.codes
         # Add dots to bar chart
-
-        not_nan_indices = pd.notna(dots["avgs"])
-        x_vals = dot_x_pos[not_nan_indices]
-        y_vals = dots["avgs"][not_nan_indices]
-        if len(y_vals) != 0:
-            sns.stripplot(x=x_vals, y=y_vals, ax=ax, color="black", jitter=0.3, size=7)
-        else:
-            print("All values were none. No dots to add.")
+        sns.stripplot(
+            x=dot_x_pos, y=dots["avgs"], ax=ax, color="black", jitter=0.3, size=7
+        )
 
     ax.axhline(0.5, linestyle="--", color="grey")
     ax.set_xlabel("Test Condition", fontweight="bold", fontsize=14)
@@ -180,16 +186,20 @@ def make_bar_charts(
 def agent_bar_charts(
     data: pd.DataFrame, results_dir: Path, chick_data: pd.DataFrame, color_bars: bool
 ):
+    # Get the imprinting condition x agent combination for each row
+    data["imp_agent"] = data["imprint.cond"] + "_" + data["agent"].astype(str)
+    # create a bar chart for each imprinting condition x agent combination
     for imp_agent in data["imp_agent"].unique():
+        # get all of the data for the current imprinting condition x agent combination
         bar_data = data[data["imp_agent"] == imp_agent]
+        # calculate the error bars
         bar_data["error_min"] = bar_data["avgs"] - bar_data["se"]
         bar_data["error_max"] = bar_data["avgs"] + bar_data["se"]
+        # create the bar chart
         make_bar_charts(
             data=bar_data,
             dots=None,
             y_col="avgs",
-            error_min_col="error_min",
-            error_max_col="error_max",
             img_name=results_dir / f"{imp_agent}_test.png",
             chick_data=chick_data,
             color_bars=color_bars,
@@ -197,8 +207,7 @@ def agent_bar_charts(
 
 
 def stats_by_imprint_cond(data: pd.DataFrame, results_dir: Path):
-    grouped_imp = data.groupby(["imprint.cond", "test.cond"])
-    by_imp_cond = grouped_imp.apply(lambda g: _stats(g, column="avgs")).reset_index()
+    by_imp_cond = _stats_by_group(data, ["imprint.cond", "test.cond"], column="avgs")
     by_imp_cond.to_csv(results_dir / "stats_by_imp_cond.csv", index=False)
     return by_imp_cond
 
@@ -224,8 +233,6 @@ def imprint_cond_bar_charts(
             data=bar_data,
             dots=dot_data,
             y_col="avgs",
-            error_min_col="error_min",
-            error_max_col="error_max",
             img_name=img_name,
             chick_data=chick_data,
             color_bars=color_bars,
@@ -233,13 +240,8 @@ def imprint_cond_bar_charts(
 
 
 def stats_overall(data: pd.DataFrame, results_dir: Path):
-    across_imp_cond = (
-        data[data["test.cond"] != "Rest"]
-        .groupby("test.cond")
-        .apply(lambda g: _stats(g, column="avgs"))
-        .drop("Rest")
-        .reset_index()
-    )
+    data_no_rest = data[data["test.cond"] != "Rest"]
+    across_imp_cond = _stats_by_group(data_no_rest, ["test.cond"], column="avgs")
     across_imp_cond.to_csv(results_dir / "stats_across_all_agents.csv", index=False)
     return across_imp_cond
 
@@ -255,17 +257,13 @@ def all_cond_bar_chart(
         data=across_imp_cond,
         dots=dot_data,
         y_col="avgs",
-        error_min_col="error_min",
-        error_max_col="error_max",
         img_name=img_name,
         chick_data=chick_data,
         color_bars=color_bars,
     )
 
 
-def test_viz(
-    results_dir: Path, chick_file: Path, bar_order="default", color_bars=False
-):
+def test_viz(results_dir: Path, chick_file: Path, bar_order="default", color_bars=True):
     # Do not warn about chained assignments
     pd.options.mode.chained_assignment = None
     # Load data
@@ -289,31 +287,10 @@ def test_viz(
     )
 
     print("Adjusting bar order...")
-    # test_data['test.cond'] = sort_cond(test_data, bar_order)
-    if bar_order == "desc":
-        order = (
-            test_data.groupby("test.cond")["percent_correct"]
-            .mean()
-            .sort_values(ascending=False)
-            .index.tolist()
-        )
-    elif bar_order == "asc":
-        order = (
-            test_data.groupby("test.cond")["percent_correct"]
-            .mean()
-            .sort_values()
-            .index.tolist()
-        )
-    elif bar_order != "default":
-        order = [x.strip() for x in bar_order.split(",")]
-    else:
-        order = test_data["test.cond"].unique().tolist()
-    test_data["test.cond"] = pd.Categorical(
-        test_data["test.cond"], categories=order, ordered=True
-    )
+    test_data["test.cond"] = sort_cond(test_data, bar_order)
 
     print("Computing statistics by agent...")
-    by_test_cond = compute_stats(test_data, results_dir)
+    by_test_cond = _compute_agent_stats(test_data, results_dir)
 
     print("Creating bar charts by agent...")
     agent_bar_charts(by_test_cond, results_dir, chick_data, color_bars)
@@ -327,7 +304,6 @@ def test_viz(
     )
 
     print("Computing statistics across all imprinting conditions...")
-
     across_imp_cond = stats_overall(data=by_test_cond, results_dir=results_dir)
 
     print("Creating bar chart for all imprinting conditions...")
