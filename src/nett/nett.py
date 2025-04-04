3d4b3bcf8824143a5846ab32fee5d4deaeca4316
Zach-Attach <zachlaborde93@gmail.com>
2025-04-04 17:51:02 -0400

    Fix to loading bar estimate

--- src/nett/nett.py
+++ src/nett/nett.py
@@ -292,9 +292,13 @@ class NETT:
         )
 
         # create loading bar
-        num_steps = steps_per_episode * (
-            len(tasklist.tasks) * episodes.get("train", 0)
-            + episodes.get("test", 0) * base_env.num_test_conditions
+        num_steps = (
+            steps_per_episode
+            * len(tasklist.tasks)
+            * (
+                episodes.get("train", 0)
+                + episodes.get("test", 0) * base_env.num_test_conditions
+            )
         )
         self.executor.loading_bar.add(name, num_steps)
         # validate tasks
